0b83c4116150c665fdbcf917744295c48475ddea
pavan teja <pavanteja6690@gmail.com>
2021-06-24 11:12:31 +0530

    done

--- index.js
+++ index.js
@@ -1,19 +1,50 @@
 var toggle = 1;
 $(document).ready(() => {
+  $("body>*:not(.sidebar):not(.navbar)").click(() => {
+    if (toggle == 0) {
+      $(".sidebar").animate({ left: "-320px" });
+      $("body").css("overflow", "scroll");
+      $("body > *:not(.sidebar):not(.navbar)").css("filter", "blur(0)");
+      toggle = 1;
+    }
+  });
   $(".hamicon").click(() => {
     // $(".sidebar").toggle("slow");
     if (toggle == 0) {
-      $(".sidebar").animate({ left: "-400px" });
+      $(".sidebar").animate({ left: "-320px" });
+      $("body").css("overflow", "scroll");
+      $("body > *:not(.sidebar):not(.navbar)").css("filter", "blur(0)");
       toggle = 1;
     } else {
       $(".sidebar").animate({ left: "0px" });
+      $("body").css("overflow", "hidden");
+      $("body > *:not(.sidebar):not(.navbar)").css("filter", "blur(3px)");
+
       toggle = 0;
     }
   });
-  $(".navsecondpart").click(() => {
-    $(".ellipsismenu").slideToggle("slow");
+  $(".submit").click(() => {
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var number = $("#number").val();
+    var subject = $("#subject").val();
+    var textmessage = $("#textmessage").val();
+    var data = { name, mail: email, subject, textmessage };
+
+    $.ajax({
+      url: "http://localhost:5000/sendmessage",
+      type: "post",
+      data: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json", //for object property name, use quoted notation shown in second
+      },
+      dataType: "json",
+      success: function (data) {
+        console.log("message was sent");
+      },
+    });
   });
-  $(".searchbutton").click(() => {
-    $.get("", (data) => {});
+  $(".navsecondpart").click(() => {
+    $(".ellipsismenu").slideToggle();
   });
 });
