de635548c1d9cbfd4f30a49d4825c1fec9f82cf8
pavan teja <pavanteja6690@gmail.com>
2021-06-25 11:23:44 +0530

    done

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var toggle = 1;
+
 $(document).ready(() => {
   $("body>*:not(.sidebar):not(.navbar)").click(() => {
     if (toggle == 0) {
       $(".sidebar").animate({ left: "-320px" });
       $("body").css("overflow", "scroll");
-      $("body > *:not(.sidebar):not(.navbar)").css("filter", "blur(0)");
+      $("body > *:not(.sidebar):not(.navbar)").css("opacity", "1");
       toggle = 1;
     }
   });
@@ -13,26 +14,25 @@ $(document).ready(() => {
     if (toggle == 0) {
       $(".sidebar").animate({ left: "-320px" });
       $("body").css("overflow", "scroll");
-      $("body > *:not(.sidebar):not(.navbar)").css("filter", "blur(0)");
+      $("body > *:not(.sidebar):not(.navbar)").css("opacity", "1");
       toggle = 1;
     } else {
       $(".sidebar").animate({ left: "0px" });
       $("body").css("overflow", "hidden");
-      $("body > *:not(.sidebar):not(.navbar)").css("filter", "blur(3px)");
+      $("body > *:not(.sidebar):not(.navbar)").css("opacity", "0.4");
 
       toggle = 0;
     }
   });
   $(".submit").click(() => {
-    var name = $("#name").val();
-    var email = $("#email").val();
-    var number = $("#number").val();
-    var subject = $("#subject").val();
-    var textmessage = $("#textmessage").val();
-    var data = { name, mail: email, subject, textmessage };
-
+    var to = $(".to").val();
+    console.log(to);
+    console.log(simplemde.value());
+    var mail = simplemde.value();
+    var data = { to, mail };
+    console.log("entered");
     $.ajax({
-      url: "https://ancient-dusk-88387.herokuapp.com/sendmessage",
+      url: "http://localhost:5000/sendmail",
       type: "post",
       data: JSON.stringify(data),
       headers: {
@@ -43,6 +43,14 @@ $(document).ready(() => {
         console.log("message was sent");
       },
     });
+    $(".notification").text("message sent").addClass("notification-temp");
+    $(".notification").slideDown();
+    $(".to").val("0");
+    simplemde.value("");
+    setInterval(() => {
+      $(".notification").text("").removeClass("notification-temp");
+      $(".notification").slideUp();
+    }, 3000);
   });
   $(".navsecondpart").click(() => {
     $(".ellipsismenu").slideToggle();
